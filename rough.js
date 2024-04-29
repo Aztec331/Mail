94d3983e62160653c7ebc3b0b67e2d4657ca2551
Aztec331 <adityabuzz331@gmail.com>
2024-04-29 20:08:07 +0530

    added valueable comments

--- rough.js
+++ rough.js
@@ -1,10 +1,15 @@
 
-
+//arrow functions in javascript
 let moltiply= (a,b) =>{
     let result = a*b
     return result
 }
 
+function moltiply2(a,b){
+    return a*b
+}
+
 
 
 console.log(moltiply(2,3))
+console.log(moltiply2(2,3))
