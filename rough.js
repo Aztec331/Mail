63c82706e996849b8e14c91ddba37bc6c3c87041
Aztec331 <adityabuzz331@gmail.com>
2024-05-04 12:57:35 +0530

    added imp comments

--- rough.js
+++ rough.js
@@ -1,20 +1,33 @@
+// GET /emails/<str:mailbox>
 
-//arrow functions in javascript
-let moltiply= (a,b) =>{
-    let result = a*b
-    return result
+// Sending a GET request to /emails/<mailbox> where <mailbox> is either inbox,
+// sent, or archive will return back to you (in JSON form) a list of all emails
+// in that mailbox, in reverse chronological order. For example, if you send a
+// GET request to /emails/inbox, you might get a JSON response like the below
+// (representing two emails):
+
+fetch('/emails/inbox')
+.then(response => response.json())
+.then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // ... do something else with emails ...
+});
+
+// GET /emails/<int:email_id>
+
+// Sending a GET request to /emails/email_id where 
+// email_id is an integer id for an email will return a 
+// JSON representation of the email, like the below:
+
+{
+        "id": 100,
+        "sender": "foo@example.com",
+        "recipients": ["bar@example.com"],
+        "subject": "Hello!",
+        "body": "Hello, world!",
+        "timestamp": "Jan 2 2020, 12:00 AM",
+        "read": false,
+        "archived": false
 }
-
-function moltiply2(a,b){
-    return a*b
-}
-
-
-
-console.log(moltiply(2,3))
-console.log(moltiply2(2,3))
-
-
-let khujli = ()=> 2*3
-
-console.log(khujli())
